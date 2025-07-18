5d69cd6c17b8d751d3c05e50e00760c965dc559e
stan99 <stan99-cheb@yandex.ru>
2025-07-18 21:12:19 +0300

    refactor: оптимизация

--- src/page/Foreign/foreign.tsx
+++ src/page/Foreign/foreign.tsx
@@ -1,32 +1,14 @@
-import { fetchPosts, postsActions } from '../../store/slices/postsSlice';
-import { getCurrentDate } from '../../utils/utils';
 import { selectors } from '../../store/selectors';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { useEffect } from 'react';
-import InfiniteList from '../../components/InfiniteList/infinite-list';
-import Loader from '../../components/Loader/loader';
+import CategoryFeed from '../../components/CategoryFeed/category-feed';
 import styles from './foreign.module.css';
 
 const Foreign = () => {
-  const dispatch = useAppDispatch();
-  const foreignPostsGroupedByDate = useAppSelector(selectors.postsUI.foreignPostsGroupedByDate);
-  const isLoading = useAppSelector(selectors.postsUI.isLoading);
-
-  useEffect(() => {
-    void dispatch(fetchPosts(getCurrentDate()));
-
-    return () => {
-      void dispatch(postsActions.clearPosts());
-    };
-  }, [dispatch]);
-
   return (
-    <main className={styles.main}>
-      <InfiniteList
-        groups={foreignPostsGroupedByDate}
-      />
-      {isLoading && <Loader />}
-    </main>
+    <CategoryFeed
+      groupSelector={selectors.postsUI.foreignPostsGroupedByDate}
+      isLoadingSelector={selectors.postsUI.isLoading}
+      extraClass={styles.main}
+    />
   );
 }
 
--- src/page/Science/science.tsx
+++ src/page/Science/science.tsx
@@ -1,32 +1,14 @@
-import { fetchPosts, postsActions } from '../../store/slices/postsSlice';
-import { getCurrentDate } from '../../utils/utils';
 import { selectors } from '../../store/selectors';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { useEffect } from 'react';
-import InfiniteList from '../../components/InfiniteList/infinite-list';
-import Loader from '../../components/Loader/loader';
+import CategoryFeed from '../../components/CategoryFeed/category-feed';
 import styles from './science.module.css';
 
 const Science = () => {
-  const dispatch = useAppDispatch();
-  const sciencePostsGroupedByDate = useAppSelector(selectors.postsUI.sciencePostsGroupedByDate);
-  const isLoading = useAppSelector(selectors.postsUI.isLoading);
-
-  useEffect(() => {
-    void dispatch(fetchPosts(getCurrentDate()));
-
-    return () => {
-      void dispatch(postsActions.clearPosts());
-    };
-  }, [dispatch]);
-
   return (
-    <main className={styles.main}>
-      <InfiniteList
-        groups={sciencePostsGroupedByDate}
-      />
-      {isLoading && <Loader />}
-    </main>
+    <CategoryFeed
+      groupSelector={selectors.postsUI.sciencePostsGroupedByDate}
+      isLoadingSelector={selectors.postsUI.isLoading}
+      extraClass={styles.main}
+    />
   );
 }
 
